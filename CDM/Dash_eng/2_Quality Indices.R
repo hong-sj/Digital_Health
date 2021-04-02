3efd5e8777f523ac5e574bd3c311ec83ef66971f
Sungjun Hong <hsj2864@gmail.com>
2021-04-02 21:18:33 +0900

    change process CDM mapping

--- CDM/Dash_eng/2_Quality Indices.R
+++ CDM/Dash_eng/2_Quality Indices.R
@@ -17,21 +17,29 @@ cdm <- read.csv("cdm_mapping.csv")
 
 #---- 1. Bed Occupancy Index ----
 # select necessary variables and generate time variables
-df <- cdm %>% 
+# -> Re-separation to visit result and admission location
+df <- cdm %>%
   select(person_id, visit_start_datetime, visit_end_datetime,
          value_as_number, condition_source_value,
-         discharge_to_source_value, visit_detail_source_value) %>%
-  mutate(year=year(visit_start_datetime), mon=month(visit_start_datetime),
-         hour=round(as.numeric(difftime(visit_end_datetime, visit_start_datetime, units = 'hours')),1))
+         discharge_to_source_value, visit_source_value) %>%
+  mutate(year = year(visit_start_datetime),
+         month = month(visit_start_datetime),
+         hour = round(as.numeric(difftime(visit_end_datetime, visit_start_datetime, units = 'hours')),1),
+         discharge = ifelse(substr(discharge_to_source_value,1,16)=='Hospital patient',
+                            'Hospital patient',
+                            discharge_to_source_value),
+         admission = ifelse(discharge=='Hospital patient', 
+                            substr(discharge_to_source_value,18,nchar(discharge_to_source_value)),
+                            NA))
 
 #formula = {(number of patients * average length of stay) / (total number of bed * days * 24hours)} * 100
-BOI <- df %>% select(person_id, year, mon, hour) %>% group_by(year, mon) %>%
+BOI <- df %>% select(person_id, year, month, hour) %>% group_by(year, month) %>%
   summarise(mt = round(mean(hour, na.rm=T),1), np = length(person_id)) %>%   # average length of stay in ED
-  mutate(nb = 73,                                                            # total number of bed in ER
-         nm = ifelse(mon%in%c(1,3,5,7,8,10,12),31,                           # generate days by month
-                     ifelse(mon%in%c(4,6,9,11),30,
-                            ifelse(as.numeric(year)%%4==0,29,28))),          # consider a leap month
-         bed = round({(np*mt)/(nb*nm*24)}*100,1)) %>%                        # calculate index
+  mutate(nb = 73,                                                                      # total number of bed in ER
+         nm = ifelse(month%in%c(1,3,5,7,8,10,12),31,                                   # generate days by month
+                     ifelse(month%in%c(4,6,9,11),30,
+                            ifelse(as.numeric(year)%%4==0,29,28))),                    # consider a leap month
+         bed = round({(np*mt)/(nb*nm*24)}*100,1)) %>%                                  # calculate index
   as.data.frame()
 
 BOI
@@ -39,52 +47,50 @@ BOI
 
 
 #---- 2. long-term patients Index ----
-# remove DOA
-i1 <- df %>% filter(condition_source_value == 63238001) %>% select(person_id) # DOA 3
-df1 <- df %>% filter(!person_id %in% i1$person_id) # 72-3=69
-df1 <- df1 %>% filter(is.na(value_as_number)==F)
-rm(i1)
-
 # derived variable: long-term ED time>12hr & >24hr & >48hr
-tmp1 <- df1 %>% mutate(ot = ifelse(hour>12&hour<=24,12,
+df1 <- df %>% mutate(ot = ifelse(hour>12&hour<=24,12,
                                    ifelse(hour>24&hour<=48,24,
                                           ifelse(hour>48,48,NA))))
-with(data=tmp1, table(ot,mon,year))
-a <- with(data=tmp1,table(ot,mon,year)) %>% as.data.frame()
+# remove DOA & missing KTAS-level
+df2 <- df1 %>% filter(is.na(condition_source_value)==T) # DOA
+df2 <- df2 %>% filter(is.na(value_as_number)==F) # KTAS
+
+with(data=df2, table(ot,month,year))
+a <- with(data=df2,table(ot,month,year)) %>% as.data.frame()
 a18 <- a%>%filter(year==2018)
 a19 <- a%>%filter(year==2019)
 a20 <- a%>%filter(year==2020)
 
 # calculate the number of patients in long-term ED time (>12hr, >24hr, >48hr)
-a18_ <- dcast(data=a18,year+mon~ot, value.var='Freq')
+a18_ <- dcast(data=a18,year+month~ot, value.var='Freq')
 a18_$t12 <- apply(a18_[,c(3:5)], 1, sum, na.rm=T)
 a18_$t24 <- apply(a18_[,c(4:5)], 1, sum, na.rm=T)
 a18_$t48 <- a18_$`48`
 a18_ <- a18_ %>% select(1,2,6:8)
 
-a19_ <- dcast(data=a19,year+mon~ot, value.var='Freq')
+a19_ <- dcast(data=a19,year+month~ot, value.var='Freq')
 a19_$t12 <- apply(a19_[,c(3:5)], 1, sum, na.rm=T)
 a19_$t24 <- apply(a19_[,c(4:5)], 1, sum, na.rm=T)
 a19_$t48 <- a19_$`48`
 a19_ <- a19_ %>% select(1,2,6:8)
 
-a20_ <- dcast(data=a20,year+mon~ot, value.var='Freq')
+a20_ <- dcast(data=a20,year+month~ot, value.var='Freq')
 a20_$t12 <- apply(a20_[,c(3:5)], 1, sum, na.rm=T)
 a20_$t24 <- apply(a20_[,c(4:5)], 1, sum, na.rm=T)
 a20_$t48 <- a20_$`48`
 a20_ <- a20_ %>% select(1,2,6:8)
 
-t18_ <- melt(data=a18_, id.vars=c('year','mon'), variable.name='ot',value.name='value')
-t18 <- dcast(data=t18_,year+ot~mon, value.var='value')
-t19_ <- melt(data=a19_, id.vars=c('year','mon'), variable.name='ot',value.name='value')
-t19 <- dcast(data=t19_,year+ot~mon, value.var='value')
-t20_ <- melt(data=a20_, id.vars=c('year','mon'), variable.name='ot',value.name='value')
-t20 <- dcast(data=t20_,year+ot~mon, value.var='value')
+t18_ <- melt(data=a18_, id.vars=c('year','month'), variable.name='ot',value.name='value')
+t18 <- dcast(data=t18_,year+ot~month, value.var='value')
+t19_ <- melt(data=a19_, id.vars=c('year','month'), variable.name='ot',value.name='value')
+t19 <- dcast(data=t19_,year+ot~month, value.var='value')
+t20_ <- melt(data=a20_, id.vars=c('year','month'), variable.name='ot',value.name='value')
+t20 <- dcast(data=t20_,year+ot~month, value.var='value')
 rm(a,a18,a19,a20,a18_,a19_,a20_,t18_,t19_,t20_)
 
 # total patients
-tn <- df1 %>% group_by(year, mon) %>% summarise(np = length( person_id)) %>% as.data.frame()
-tnn <- dcast(data=tn,year~mon, value.var='np')
+tn <- df1 %>% group_by(year, month) %>% summarise(np = length( person_id)) %>% as.data.frame()
+tnn <- dcast(data=tn,year~month, value.var='np')
 tnn <- tnn %>% mutate(ot='all')
 tnn <- tnn%>%select(1,14,2:13)
 
@@ -92,7 +98,7 @@ tt18 <- rbind(t18, subset(tnn,year==2018))
 tt19 <- rbind(t19, subset(tnn,year==2019))
 tt20 <- rbind(t20, subset(tnn,year==2020))
 index <- rbind(tt18,tt19,tt20);rownames(index) <- NULL
-rm(t18,t19,t20,tn,tnn,tt18,tt19,tt20,tmp1)
+rm(t18,t19,t20,tn,tnn,tt18,tt19,tt20)
 
 # 2018
 p18 <- subset(index,year==2018);head(p18)
@@ -126,10 +132,10 @@ rm(index, index1)
 # use previous total number of patients
 t_time <- LPI %>% filter(ot=='all')
 t_time <- t_time %>% rename(value_as_number=ot)
-kt_ <- df1 %>% group_by(year,mon, value_as_number) %>% summarise(N=n()) %>% as.data.frame()
+kt_ <- df2 %>% group_by(year, month, value_as_number) %>% summarise(N=n()) %>% as.data.frame()
 
 # reshape data structure
-kt <- dcast(data=kt_,year+value_as_number~mon, value.var='N')
+kt <- dcast(data=kt_,year+value_as_number~month, value.var='N', fill=0)
 rm(kt_)
 
 k18 <- rbind(subset(kt, year==2018), subset(t_time, year==2018))
@@ -172,34 +178,37 @@ rm(index, index1, index2)
 
 #---- 4. Rate of ER Discharge ----
 # check discharge type
-table(df$discharge_to_source_value)
+table(df$discharge)
 
 # calculate rates
-tmp <- df %>% mutate(n=1) %>% group_by(year, mon, discharge_to_source_value) %>% summarise(n1=sum(n)) %>% as.data.frame()
-tn <- tmp %>% group_by(year, mon) %>% summarise(tn = sum(n1)) %>% as.data.frame();head(tn)
-index <- tmp %>% left_join(tn, by=c('year','mon')) %>% rename(visit_result = discharge_to_source_value)
+tmp <- df %>% mutate(n=1) %>% group_by(year, month, discharge) %>% summarise(n1=sum(n)) %>% as.data.frame()
+tn <- tmp %>% group_by(year, month) %>% summarise(tn = sum(n1)) %>% as.data.frame();head(tn)
+index <- tmp %>% left_join(tn, by=c('year','month')) %>% rename(visit_result = discharge)
 index <- index %>% mutate(rate = round(n1/tn*100,2))
 head(index)
 rm(tmp, tn)
 
 # change name shortly
-index$visit_result <- ifelse(index$visit_result=='Patient transfer to another hospital','Hospital transfer',
-                             ifelse(index$visit_result=='Hospital admission','Admission',index$visit_result))
+index$visit_result <- ifelse(index$visit_result=='Patient transfer from hospital to hospital',
+                             'Hospital transfer',
+                             ifelse(index$visit_result=='Hospital patient',
+                                    'Admission',
+                                    index$visit_result))
 Discharge<-index
-
+Discharge
 rm(index)
 
 
 
 #---- 5. Rate of Hospital Admission ----
 # check discharge type
-table(df$visit_detail_source_value)
+table(df$admission)
 
 # calculate rates
-tmp <- df %>% filter(discharge_to_source_value=='Hospital admission') %>% 
-  mutate(n=1) %>% group_by(year, mon, visit_detail_source_value) %>% summarise(n1=sum(n)) %>% as.data.frame()
-tn <- tmp %>% group_by(year, mon) %>% summarise(tn = sum(n1)) %>% as.data.frame();head(tn)
-index <- tmp %>% left_join(tn, by=c('year','mon')) %>% rename(location = visit_detail_source_value)
+tmp <- df %>% filter(discharge=='Hospital patient') %>% 
+  mutate(n=1) %>% group_by(year, month, admission) %>% summarise(n1=sum(n)) %>% as.data.frame()
+tn <- tmp %>% group_by(year, month) %>% summarise(tn = sum(n1)) %>% as.data.frame();head(tn)
+index <- tmp %>% left_join(tn, by=c('year','month')) %>% rename(location = admission)
 index <- index %>% mutate(rate = round(n1/tn*100,2))
 head(index)
 rm(tmp, tn)
@@ -208,7 +217,7 @@ rm(tmp, tn)
 index$location <- ifelse(index$location=='Ward','GW',
                          ifelse(index$location=='Intensive Care','ICU','OP'))
 Admission<-index
-
+Admission
 rm(index)
 
 
